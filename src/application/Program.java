35dd56c3f9d96ade55a32b5ab138e3228701be9c
vfdesouza <vfdesouza@outlook.com>
2022-04-25 21:01:33 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
 import model.entities.Departament;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
         Departament departament = new Departament(1, "Books");
+        Seller seller = new Seller(1, "Vinicius", "vf.com", new Date(),4000.0);
         System.out.println(departament);
+        System.out.println(seller);
     }
 }
