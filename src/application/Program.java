d8637a5497e06c6b86d742bee448480e19142e0e
vfdesouza <vfdesouza@outlook.com>
2022-04-30 08:43:55 -0300

    DepartmentDaoJDBC class implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,37 @@
 package application;
-
-import db.DB;
+import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
-import model.impl.SellerDaoJDBC;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
-        List<Seller> list = new ArrayList<>();
-        SellerDao sellerDao = new SellerDaoJDBC(DB.getConnection());
-        list = sellerDao.findAll();
 
-        for(Seller obj : list) {
-            System.out.println(obj);
-        }
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller;
+
+        Date birthDate = new Date();
+
+        //Teste de 'insert' no banco de dados, na tabela "Seller";
+
+        seller = new Seller(null, "Vinícius Souza", "vf.com", birthDate, 4000.0, new Department(2, null));
+        sellerDao.insert(seller);
+
+        System.out.println("Objeto inserido no banco de dados!");
+
+        //Teste de 'delete' no banco de dados, na tabela "Seller" utilizando um 'id' como referência para exclusão;
+
+        sellerDao.deleteById(11);
+
+        System.out.println("Objeto excluído do banco de dados!");
+
+        //Teste de 'update' no banco de dados, na tabela "Seller" utilizando um 'id' como referência para atualização;
+
+        seller = sellerDao.findById(1);
+        seller.setName("Vinícius Ferreira de Souza");
+        sellerDao.update(seller);
+        System.out.println("Atualizado com sucesso!");
     }
 }
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,15 @@
 package model.dao;
 
 import db.DB;
+import model.impl.DepartmentDaoJDBC;
 import model.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDao() {
         return new SellerDaoJDBC(DB.getConnection());
+    }
+    public static DepartmentDao creDepartamentDao() {return new DepartmentDaoJDBC(DB.getConnection());
+
     }
 }
